e498e03716b8410eb1653b5c687e48bc43f47c25
Md Razwan Niam <razwanniam1@gmail.com>
2023-12-27 11:17:45 +0600

    vector initialization

---
+++ STL Vector/vector_initialization.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // vector<int> v; //type 1
+    // vector<int> v(5, 7);
+    // vector<int> v2(v);
+    // int a[6] = {0, 1, 2, 3, 4, 5};
+    // vector<int> v3(a, a + 6);
+
+    vector<int> v4 = {2, 10, 3};
+    for (int i = 0; i < v4.size(); i++)
+    {
+        cout << v4[i] << " ";
+    }
+    cout << endl;
+
+    cout << v4.size() << endl;
+
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     cout << v[i] << endl;
+    // }
+
+    // cout << v3.size() << endl;
+    return 0;
+}
