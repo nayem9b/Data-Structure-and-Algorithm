c8a38b0e71202a72b76c600a5663047ac27397ed
Md Razwan Niam <razwanniam1@gmail.com>
2024-01-12 22:33:50 +0600

    input of dll

--- Doubly Linked List/input.cpp
+++ Doubly Linked List/input.cpp
@@ -42,7 +42,7 @@ void insert_tail(Node *&head, Node *&tail, int val)
         tail = newNode;
         return;
     }
-    tail->next = newNode;
+    // tail->next = newNode;
     newNode->prev = tail;
     tail = tail->next;
 }
