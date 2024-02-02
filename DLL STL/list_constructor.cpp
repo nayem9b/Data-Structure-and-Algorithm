6af546f2a67f358e118765a1969b3423bf62ae72
Md Razwan Niam <razwanniam1@gmail.com>
2024-02-02 16:37:40 +0600

    delete node and list modifiers

--- DLL STL/list_constructor.cpp
+++ DLL STL/list_constructor.cpp
@@ -4,12 +4,12 @@ int main()
 {
     // list<int> myList;
     // list<int> list2 = {1, 2, 3, 4, 5};
-    int a[5] = {10, 20, 30, 40, 50};
-    vector<int> v = {100, 200, 300};
-    // list<int> myList(10, 5);
+    // int a[5] = {10, 20, 30, 40, 50};
+    // vector<int> v = {100, 200, 300};
+    list<int> myList(10, 5);
     // list<int> myList(list2);
     // list<int> myList(a, a + 5);
-    list<int> myList(v.begin(), v.end());
+    // list<int> myList(v.begin(), v.end());
 
     // for (auto it = myList.begin(); it != myList.end(); it++)
     // {
--- DLL STL/modifiers_function.cpp
+++ DLL STL/modifiers_function.cpp
@@ -8,16 +8,16 @@ int main()
     // myList.pop_back();
     // myList.pop_back();
     // myList.pop_front();
-    // myList.insert(next(myList.begin(), 3), 100);
+    myList.insert(next(myList.begin(), 3), 100);
     // myList.erase(next(myList.begin(), 2));
     // myList.insert(next(myList.begin(), 3), v.begin(), v.end());
     // myList.erase(next(myList.begin(), 2), next(myList.begin(), 5));
     // replace(myList.begin(), myList.end(), 30, 100);
 
-    // for (int val : myList)
-    // {
-    //     cout << val << endl;
-    // }
+    for (int val : myList)
+    {
+        cout << val << endl;
+    }
 
     auto it = find(myList.begin(), myList.end(), 60);
     if (it == myList.end())
