0a9b4d22bd34b98bda33adfd813cc84cbd79e75c
nayem9b <razwanniam1@gmail.com>
2024-04-17 06:53:59 +0600

    count nodes and leaf nodes

---
+++ Binary Tree/count_leaf_node.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+        // Jabotiyo ja kaaj
+        int l, r;
+        cin >> l >> r;
+        Node *left;
+        Node *right;
+        if (l == -1)
+            left = NULL;
+        else
+            left = new Node(l);
+        if (r == -1)
+            right = NULL;
+        else
+            right = new Node(r);
+        p->left = left;
+        p->right = right;
+        // children gulo ke push koro
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+    return root;
+};
+void level_order(Node *root)
+{
+    if (root == NULL)
+    {
+        cout << "Tree nai" << endl;
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        // 1. ber kore ana
+        Node *f = q.front();
+        q.pop();
+
+        // 2. jabotiyo ja kaj ache
+        cout << f->val << " ";
+
+        // 3. tar children gulo ke rakha
+        if (f->left)
+            q.push(f->left);
+        if (f->right)
+            q.push(f->right);
+    }
+};
+int count_leaf(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    if(root->left==NULL && root->right==NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        int l = count_leaf(root->left);
+        int r= count_leaf(root->right);
+        return l+r;
+    }
+};
+int main()
+{
+    Node *root = input_tree();
+    cout << count_leaf(root) << endl;
+    return 0;
+}
